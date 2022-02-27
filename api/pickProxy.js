66e9e74f22f0a7104bba0a7f87e61261a006daa1
rechnash <99588320+rechnash@users.noreply.github.com>
2022-02-27 16:49:01 -0300

    Update pickProxy.js

--- api/pickProxy.js
+++ api/pickProxy.js
@@ -60,7 +60,7 @@ async function getProxyFlow () {
 
         let res = await request({
             url,
-            proxy: pickProxy('server'),
+            proxy: await pickProxy('server'),
             userAgent: $g.pickUserAgent('server'),
             json: true
         })
